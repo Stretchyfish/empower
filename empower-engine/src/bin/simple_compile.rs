da4b6821321e0efbce7ee990ad9901eeef6bb952
Stretchyfish <m.maarssoe@gmail.com>
2025-06-24 08:30:10 +0200

    modified empower-engine to work with empower node graph in a seperate crate

--- empower-engine/src/bin/simple_compile.rs
+++ empower-engine/src/bin/simple_compile.rs
@@ -1,32 +1,36 @@
-use empower_engine::EmpowerEngine;
+use empower_node_graph::EmpowerNodeGraph;
+use empower_node_graph::NodeType;
+use empower_node_graph::EmpowerData;
+use empower_engine;
 
 fn main()
 {
-    let mut engine = EmpowerEngine::new();
+    // let mut engine = EmpowerEngine::new();
+    let mut node_graph = EmpowerNodeGraph::default();
 
-    let new_node_type = empower_engine::node::NodeType::IntegerVariable;
+    let new_node_type = NodeType::IntegerVariable;
 
-    let node_key_1 = engine.add_node(new_node_type.clone());
-    let node_key_2 = engine.add_node(new_node_type.clone());
-    let node_key_3 = engine.add_node(new_node_type.clone());
-    let node_key_4 = engine.add_node(new_node_type.clone());
+    let node_key_1 = node_graph.add_node(new_node_type.clone());
+    let node_key_2 = node_graph.add_node(new_node_type.clone());
+    let node_key_3 = node_graph.add_node(new_node_type.clone());
+    let node_key_4 = node_graph.add_node(new_node_type.clone());
 
-    let input_port_key_1= engine.get_node_input_port_keys(node_key_1).unwrap()[0];
-    let output_port_key_1= engine.get_node_output_port_keys(node_key_1).unwrap()[0];
+    let input_port_key_1= node_graph.get_node_input_port_keys(node_key_1).unwrap()[0];
+    let output_port_key_1= node_graph.get_node_output_port_keys(node_key_1).unwrap()[0];
 
-    let input_port_key_2= engine.get_node_input_port_keys(node_key_2).unwrap()[0];
-    let output_port_key_2= engine.get_node_output_port_keys(node_key_2).unwrap()[0];
+    let input_port_key_2= node_graph.get_node_input_port_keys(node_key_2).unwrap()[0];
+    let output_port_key_2= node_graph.get_node_output_port_keys(node_key_2).unwrap()[0];
 
-    let input_port_key_3= engine.get_node_input_port_keys(node_key_3).unwrap()[0];
-    let output_port_key_3= engine.get_node_output_port_keys(node_key_3).unwrap()[0];
+    let input_port_key_3= node_graph.get_node_input_port_keys(node_key_3).unwrap()[0];
+    let output_port_key_3= node_graph.get_node_output_port_keys(node_key_3).unwrap()[0];
 
-    let output_port_key_4 = engine.get_node_output_port_keys(node_key_4).unwrap()[0];
-    let input_port_key_4= engine.get_node_input_port_keys(node_key_4).unwrap()[0];
+    let output_port_key_4 = node_graph.get_node_output_port_keys(node_key_4).unwrap()[0];
+    let input_port_key_4= node_graph.get_node_input_port_keys(node_key_4).unwrap()[0];
 
-    engine.add_connection(input_port_key_2, output_port_key_1);
-    engine.add_connection(input_port_key_3, output_port_key_2);
-    engine.add_connection(input_port_key_4, output_port_key_2);
+    node_graph.add_connection(input_port_key_2, output_port_key_1);
+    node_graph.add_connection(input_port_key_3, output_port_key_2);
+    node_graph.add_connection(input_port_key_4, output_port_key_2);
 
-    engine.set_input_port_value(input_port_key_1, empower_engine::EmpowerData::Integer(50));
-    engine.debug_compile();
+    node_graph.set_input_port_value(input_port_key_1, EmpowerData::Integer(50));
+    empower_engine::debug_compile(&mut node_graph);
 }
--- empower-engine/src/port/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod input_port;
-pub mod output_port;
