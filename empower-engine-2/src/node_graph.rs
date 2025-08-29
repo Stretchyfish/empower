f5acade97e638ad881a62906db94ddcc259cb98f
Stretchyfish <m.maarssoe@gmail.com>
2025-08-29 08:44:02 +0200

    began initial execution workflow setup in node graph

--- empower-engine-2/src/node_graph.rs
+++ empower-engine-2/src/node_graph.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::VecDeque;
 
 pub type NodeGraphKey = i32;
 
@@ -9,10 +10,7 @@ pub use node::NodeKind;
 
 mod port;
 use port::Port;
-use port::PortCompatability;
 use port::PortValue;
-
-mod execution;
 
 pub struct NodeGraph
 {
@@ -152,9 +150,48 @@ impl NodeGraph
 
     pub fn execute_node_graph(&mut self) -> bool
     {
-        execution::execute_node_graph(self);
+        if self.node_count() == 0 { return false; }
+
+        let start_node_key: NodeGraphKey = 1; // @TODO, find a better approach
+        self.execute_node_graph_from_entry(&start_node_key)
+    }
+
+    pub fn execute_node_graph_from_entry(&mut self, node_key: &NodeGraphKey) -> bool
+    {
+        if !self.nodes.contains_key(node_key) { return false; }
+
+        let mut node_keys_to_execute_queue: VecDeque<NodeGraphKey> = VecDeque::new();
+        node_keys_to_execute_queue.push_back( *node_key );
+    
+        while !node_keys_to_execute_queue.is_empty()
+        {
+            println!("{:?}", node_keys_to_execute_queue); // @TODO, remove this and add a debug mode
+
+            let node_to_execute_key: NodeGraphKey = node_keys_to_execute_queue[0];
+
+            let next_nodes_to_execute: Vec<NodeGraphKey> = self.execute_node(&node_to_execute_key);
+
+            node_keys_to_execute_queue.extend(next_nodes_to_execute);
+            node_keys_to_execute_queue.pop_front();
+        }
 
         true
+    }
+
+    pub fn execute_node(&mut self, node_key: &NodeGraphKey) -> Vec<NodeGraphKey>
+    {
+        node::node_kind::execute_node(node_key, self);
+
+        let distribution_result = self.distribute_outputs(node_key); 
+        match distribution_result
+        {
+            Ok( new_nodes_to_execute ) => new_nodes_to_execute,
+            Err( error_text ) =>
+            {
+                println!("{}", error_text); // @TODO, printing should be part of a debug mode
+                Vec::new()
+            },
+        }
     }
 
     fn get_available_node_key(&self) -> NodeGraphKey
@@ -172,8 +209,60 @@ impl NodeGraph
         self.output_ports.keys().max().unwrap_or(&0) + 1
     }
 
-    fn distribute_outputs(&mut self, node_key: &NodeGraphKey)
+    fn distribute_outputs(&mut self, node_key: &NodeGraphKey) -> Result<Vec<NodeGraphKey>, String>
     {
-        
+        let node = match self.nodes.get(node_key)
+        {
+            Some( node ) => node,
+            None => return Err( format!("Failed to retrieve node with key {} in distribute outputs", node_key) ),
+        };
+
+        if node.output_port_keys.is_empty()
+        {
+            return Ok( Vec::new() );
+        }
+
+        let mut next_nodes_to_execute = Vec::new(); // @TODO, fill this out
+
+        for output_port_key in &node.output_port_keys
+        {
+            let connected_ports = match self.connections_out.get(output_port_key)
+            {
+                Some( connections ) => connections,
+                None => continue,
+            };
+
+            // This is a security check, but its irrelevant to check if there is no connected port
+            let output_port = match self.output_ports.get(output_port_key)
+            {
+                Some( port ) => port,
+                None => return Err( format!("Failed to retrieve output port with key {} in distribute outputs", output_port_key) ),
+            };
+
+            for connected_input_port_key in connected_ports
+            {
+                let input_port = match self.input_ports.get_mut(connected_input_port_key)
+                {
+                    Some ( port ) => port,
+                    None => return Err( format!("Failed to retrieve input port with key {} in distribute outputs", connected_input_port_key)),
+                };
+
+                if !input_port.compatability.contains_port_value_type(&output_port.value)
+                {
+                    return Err( format!("ERROR, input port {} and output port {} are connected, but have incompatable types in distribute output", input_port.key, output_port_key) );
+                }
+
+                // @TODO, find a way of detecting math / immediate nodes 
+                match input_port.value
+                {
+                    PortValue::Trigger => next_nodes_to_execute.push(input_port.node_key),
+                    _ => {},
+                }
+
+                input_port.value = output_port.value.clone();
+            }
+        }
+
+        Ok( next_nodes_to_execute )
     }
 }
--- empower-engine-2/src/node_graph/execution/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod execute_node_graph;
-pub use execute_node_graph::execute_node_graph;
