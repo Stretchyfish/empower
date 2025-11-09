7cbbef4a537a57a267edb6e3331c94d086207749
Stretchyfish <m.maarssoe@gmail.com>
2025-11-09 14:35:07 +0100

    Moved port value operator overloading

--- better-empower-engine/src/node_graph/node/port/port_value.rs
+++ better-empower-engine/src/node_graph/node/port/port_value.rs
@@ -1,4 +1,5 @@
 use std::mem::discriminant;
+use std::ops;
 
 #[derive(Default, Clone, Debug, PartialEq)]
 pub enum PortValue
@@ -17,5 +18,39 @@ impl PortValue
     pub fn is_same_type_as(&self, value: &PortValue) -> bool
     {
         discriminant(self) == discriminant(value)
+    }
+}
+
+impl ops::Add for PortValue
+{
+    type Output = PortValue;
+
+    fn add(self, rhs: Self) -> Self::Output
+    {
+        match (self, rhs)
+        {
+            ( PortValue::Integer(a), PortValue::Integer(b) ) => PortValue::Integer( a + b),
+            ( PortValue::Float(a), PortValue::Float(b) ) => PortValue::Float( a + b),
+            ( PortValue::Float(a), PortValue::Integer(b) ) => PortValue::Float( a + b as f32),
+            ( PortValue::Integer(a), PortValue::Float(b) ) => PortValue::Float( a as f32 + b),
+            _ => panic!("Tried to do addition on two incompatible types"),
+        }
+    }
+}
+
+impl ops::Mul for PortValue
+{
+    type Output = PortValue;
+
+    fn mul(self, rhs: Self) -> Self::Output 
+    {
+        match (self, rhs)
+        {
+            ( PortValue::Integer(a), PortValue::Integer(b) ) => PortValue::Integer( a * b),
+            ( PortValue::Float(a), PortValue::Float(b) ) => PortValue::Float( a * b),
+            ( PortValue::Float(a), PortValue::Integer(b) ) => PortValue::Float( a * b as f32),
+            ( PortValue::Integer(a), PortValue::Float(b) ) => PortValue::Float( a as f32 * b),
+            _ => panic!("Tried to do multiplication on two incompatible types"),
+        }
     }
 }
